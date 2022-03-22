a34212aa0c183a977064f15704a7293101f0377a
Hemant-Dev <phemant092@gmail.com>
2022-03-22 19:55:03 +0530

    Till Arrays2 Assignment - checkRotationArray

--- src/Arrays2/ArraysAssignment2.java
+++ src/Arrays2/ArraysAssignment2.java
@@ -153,6 +153,22 @@ public class ArraysAssignment2 {
         return secondMax;
     }
 
+    //Rotation Checker
+    public static int checkRotation(int[] arr)
+    {
+        int ans = 0;
+
+        for(int i=0; i<arr.length-1; i++)
+        {
+            if(arr[i] > arr[i+1]){
+                ans = i+1;
+                break;
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -197,7 +213,16 @@ public class ArraysAssignment2 {
 
 
 //        4.Check Array Rotation
-
+        int t = sc.nextInt();
+        while(t != 0)
+        {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            takeInput(arr);
+            int rot = checkRotation(arr);
+            System.out.println("Rotation: " + rot);
+            t--;
+        }
 
     }
 }
